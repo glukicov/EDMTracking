8022b562765cf0ca93ae53b7876758991b373f3b
glukicov <g.lukicov@ucl.ac.uk>
2020-03-26 20:19:49 +0000

    EDM with data

--- PYMacros/Scans_edm.py
+++ PYMacros/Scans_edm.py
@@ -2,6 +2,7 @@
 # A front-end module to run over fitWithBlinders_skim.py iteratively  
 
 import sys, re, os, subprocess, datetime, glob
+import time as timeMod
 import argparse
 import pandas as pd
 import numpy as np
@@ -20,25 +21,25 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument("--all", action='store_true', default=False) # just run all 4 DS 
 
 arg_parser.add_argument("--start", action='store_true', default=False) 
-arg_parser.add_argument("--end", action='store_true', default=False) 
+arg_parser.add_argument("--stop", action='store_true', default=False) 
 arg_parser.add_argument("--p_min", action='store_true', default=False) 
-arg_parser.add_argument("--p_both", action='store_true', default=False) 
-arg_parser.add_argument("--period", action='store_true', default=False) 
+arg_parser.add_argument("--p_minp_max", action='store_true', default=False) 
+arg_parser.add_argument("--g2period", action='store_true', default=False) 
 arg_parser.add_argument("--phase", action='store_true', default=False) 
 arg_parser.add_argument("--lt", action='store_true', default=False) 
-arg_parser.add_argument("--bins", action='store_true', default=False) 
+arg_parser.add_argument("--bin_w", action='store_true', default=False) 
 
 arg_parser.add_argument("--plot_start", action='store_true', default=False) 
-arg_parser.add_argument("--plot_end", action='store_true', default=False) 
+arg_parser.add_argument("--plot_stop", action='store_true', default=False) 
 arg_parser.add_argument("--plot_p_min", action='store_true', default=False) 
-arg_parser.add_argument("--plot_p_both", action='store_true', default=False) 
-arg_parser.add_argument("--plot_period", action='store_true', default=False) 
+arg_parser.add_argument("--plot_p_minp_max", action='store_true', default=False) 
+arg_parser.add_argument("--plot_g2period", action='store_true', default=False) 
 arg_parser.add_argument("--plot_phase", action='store_true', default=False) 
 arg_parser.add_argument("--plot_lt", action='store_true', default=False) 
-arg_parser.add_argument("--plot_bins", action='store_true', default=False) 
+arg_parser.add_argument("--plot_bin_w", action='store_true', default=False) 
 
 arg_parser.add_argument("--corr", action='store_true', default=False) 
-arg_parser.add_argument("--band_off", action='store_true', default=False) 
+arg_parser.add_argument("--band_off", action='store_true', default=True) 
 arg_parser.add_argument("--abs", action='store_true', default=False) 
 args=arg_parser.parse_args()
 
@@ -80,7 +81,7 @@ if(args.start):
     print("Start times:", start_times)
     in_=input("Start scans?")
 
-if(args.end):   
+if(args.stop):   
     end_times = np.linspace(300, 450, 9, dtype=float)
     print("End times:", end_times)
     in_=input("Start scans?")
@@ -90,14 +91,14 @@ if(args.p_min):
     print("P min:", p_min)
     in_=input("Start scans?")
 
-if(args.p_both):
+if(args.p_minp_max):
     p_min = np.linspace(0, 1000, 11, dtype=float)
     p_max = np.linspace(3100, 2100, 11, dtype=float)
     print("P min:", p_min)
     print("P max:", p_max)
     in_=input("Start scans?")
 
-if(args.period): 
+if(args.g2period): 
     period=np.linspace(g2period*(1-30e-6), g2period*(1+30e-6), 10, dtype=float)
     print("period:", period)
     in_=input("Start scans?")
@@ -112,8 +113,8 @@ if(args.lt):
     print("lifetime:", lt)
     in_=input("Start scans?")
 
-if(args.bins): 
-    bins=np.linspace(5, 26, 21, dtype=float)
+if(args.bin_w): 
+    bins=np.linspace(5, 25, 11, dtype=float)
     print("bins width [ns]:", bins)
     in_=input("Start scans?")
 
@@ -124,23 +125,23 @@ def main():
     '''
     if(args.all): all("../DATA/HDF/EDM/60h.h5", "../DATA/HDF/EDM/9D.h5", "../DATA/HDF/EDM/HK.h5", "../DATA/HDF/EDM/EG.h5")
     if(args.start): time_scan(DS_path, start_times, "--t_min")
-    if(args.end): time_scan(DS_path, end_times, "--t_max")
+    if(args.stop): time_scan(DS_path, end_times, "--t_max")
     if(args.p_min): time_scan(DS_path, p_min, "--p_min")
-    if(args.period): time_scan(DS_path, period, "--g2period")
+    if(args.g2period): time_scan(DS_path, period, "--g2period")
     if(args.phase): time_scan(DS_path, phase, "--phase")
     if(args.lt): time_scan(DS_path, lt, "--lt")
-    if(args.bins): time_scan(DS_path, bins,  "--bin_w")
+    if(args.bin_w): time_scan(DS_path, bins,  "--bin_w")
 
-    if(args.p_both): both_scan(DS_path, p_min, p_max)
+    if(args.p_minp_max): both_scan(DS_path, p_min, p_max)
     
     if(args.plot_start): plot(direction="start")
-    if(args.plot_end): plot(direction="stop")
+    if(args.plot_stop): plot(direction="stop")
     if(args.plot_p_min): plot(direction="p_min")
-    if(args.plot_period): plot(direction="g2period")
+    if(args.plot_g2period): plot(direction="g2period")
     if(args.plot_phase): plot(direction="phase")
     if(args.plot_lt): plot(direction="lt")
-    if(args.plot_bins): plot(direction="bin_w")
-    if(args.plot_p_both): plot(direction="p_min", bidir=True, second_direction="p_max")
+    if(args.plot_bin_w): plot(direction="bin_w")
+    if(args.plot_p_minp_max): plot(direction="p_min", bidir=True, second_direction="p_max")
     if(args.corr): corr()
 
 
@@ -150,21 +151,23 @@ def all(DS_path):
 
 def time_scan(DS_path, times, key_scan):
     subprocess.call(["trash"] + glob.glob("../DATA/scans/edm_scan*"))
-    subprocess.Popen( ["trash"] + glob.glob("../fig/scans/*.png") )
+    #subprocess.Popen( ["trash"] + glob.glob("../fig/scans/*.png") )
     for path in DS_path:
         for time in times:
-             subprocess.call(["python3", "getLongitudinalField.py", "--hdf", path, "--scan", key_scan, str(time)])
+             subprocess.Popen(["python3", "getLongitudinalField.py", "--hdf", path, "--scan", key_scan, str(time)])
+             timeMod.sleep(5)
 
 def both_scan(DS_path, p_min, p_max):
     subprocess.call(["trash"] + glob.glob("../DATA/scans/edm_scan*"))
-    subprocess.Popen( ["trash"] + glob.glob("../fig/scans/*.png") )
+    #subprocess.Popen( ["trash"] + glob.glob("../fig/scans/*.png") )
     for path in DS_path:
         for i, mom in enumerate(p_min):
-            subprocess.call(["python3", "getLongitudinalField.py", "--hdf", path, "--scan", "--p_min", str(p_min[i]), "--p_max",  str(p_max[i])])
+            subprocess.Popen(["python3", "getLongitudinalField.py", "--hdf", path, "--scan", "--p_min", str(p_min[i]), "--p_max",  str(p_max[i])])
+            timeMod.sleep(5)
 
 def plot(direction="start", bidir=False, second_direction=None):
     print("Making scan summary plot")
-    subprocess.Popen( ["trash"] + glob.glob("../fig/scans_fom/*.png") )
+    # subprocess.Popen( ["trash"] + glob.glob("../fig/scans_fom/*.png") )
     
 
     for i_key, key in enumerate(keys):
@@ -185,7 +188,9 @@ def plot(direction="start", bidir=False, second_direction=None):
 
         #resolve chi2 - a special parameter and add to data
         chi2 = data['chi2']
-        chi2_e=np.sqrt(2/ (data['ndf']-par_n) ) #number of bins - number of fit parameters          
+        # print(chi2)
+        chi2_e=np.sqrt(2/ (data['ndf']-par_n) ) #number of bins - number of fit parameters
+        # print(chi2_e)          
         data['chi2_e']=chi2_e # add chi2_e to the dataframe
         par_names[i_key].insert(0, "chi2")
         par_labels[i_key].insert(0, r"$\frac{\chi^2}{\rm{DoF}}$")
@@ -240,12 +245,21 @@ def plot(direction="start", bidir=False, second_direction=None):
 
                 # resolve paramters for plotting 
                 x=plot_data[direction]
+                dirName=direction
 
                 if(bidir):
+                    plot_data=plot_data.sort_values(by=['p_min'])
+                    plot_data=plot_data.reset_index()
                     x1=plot_data[direction]; x2=plot_data[second_direction]
+                    # print(x1)
+                    # print(x2)
+                    # sys.exit()
                     x=[str(int(x1[i]))+"-"+str(int(x2[i])) for i, s in enumerate(x1)]
+                    dirName=direction+second_direction
+
                 
-                if(args.plot_bins): x=x*1e3 # us to ns     
+                
+                if(args.plot_bin_w): x=x*1e3 # us to ns     
 
 
                 #loop over all parameters 
@@ -255,7 +269,7 @@ def plot(direction="start", bidir=False, second_direction=None):
                     y_e=plot_data[par_names[i_key][i_par]+'_e'] 
                     y_s = np.sqrt(y_e**2-y_e[0]**2) # 1sigma band
                     if(args.abs): y_s = np.sqrt(np.abs(y_e**2-y_e[0]**2)) # 1sigma band
-                    if(args.plot_end): y_s = np.sqrt(y_e[0]**2-y_e**2); # 1sigma band
+                    if(args.plot_stop): y_s = np.sqrt(y_e[0]**2-y_e**2); # 1sigma band
 
                     if(y_s.isnull().sum()>0): 
                         print("Error at later times are smaller - not physical, check for bad fit at these times [us]:")
@@ -265,44 +279,48 @@ def plot(direction="start", bidir=False, second_direction=None):
                     fig, ax = cu.plot(x, y, y_err=y_e, error=True, elw=2, label="S"+str(station)+": "+ds+" DS "+key_names[i_key], tight=False)
                     ax.plot(x, y, marker=".", ms=10, c="g", lw=0)
                     sigma_index=0; band_P=y[sigma_index]+y_s; band_M=y[sigma_index]-y_s;
-                    if(args.plot_end): sigma_index=len(y)-1; band_P=y[sigma_index]-np.flip(y_s); band_M=y[sigma_index]+np.flip(y_s)
+                    if(args.plot_stop): sigma_index=len(y)-1; band_P=y[sigma_index]-np.flip(y_s); band_M=y[sigma_index]+np.flip(y_s)
                     if(not args.band_off): ax.plot(x, band_P, c="r", ls=":", lw=2, label=r"$\sigma_{\Delta_{21}}$"); ax.plot(x, band_M, c="r", ls=":", lw=2)
                     # if(par_names[i_key][i_par]=='tau'): ax.plot([np.min(x)-2, np.max(x)+2], [64.44, 64.44], c="k", ls="--"); ax.set_ylim(np.min(y)-0.1, 64.6);
-                    if(par_names[i_key][i_par]=='chi2' and not args.plot_p_both): ax.plot([min(x)-2, max(x)+2], [1, 1], c="k", ls="--");
-                    if(not args.plot_p_both ): ax.set_xlim(min(x)-2, max(x)+2);
+                    if(par_names[i_key][i_par]=='chi2' and not args.plot_p_minp_max): ax.plot([min(x)-2, max(x)+2], [1, 1], c="k", ls="--");
+                    if(not args.plot_p_minp_max ): ax.set_xlim(min(x)-2, max(x)+2);
                     ax.set_xlabel(direction+r" time [$\rm{\mu}$s]", fontsize=font_size);
                     
                     if(args.plot_lt): ax.set_xlabel(r"$\tau$"+r" [$\rm{\mu}$s]", fontsize=font_size); ax.set_xlim(min(x)*0.95, max(x)*1.05)
                     if(args.plot_phase): ax.set_xlabel(r"$\phi$"+r" [rad]", fontsize=font_size); ax.set_xlim(min(x)*0.999, max(x)*1.001)
 
-                    if(args.plot_period): 
+                    if(args.plot_g2period): 
                         ax.set_xlabel(r"$T_{g-2}$ "+r"[$\rm{\mu}$s]", fontsize=font_size); ax.set_xlim(min(x)*0.99999, max(x)*1.00001)
                         x_ppm= ( (plot_data[direction]-g2period)/g2period ) * 1e6; 
                         ax2 = ax.twiny()
                         ax2.set_xlabel(r"$T_{g-2}$ "+r"(ppm)", fontsize=font_size-2);
                         ax2.plot(x_ppm, y, lw=0, label=r"$\Delta=$"+str( int((abs(max(y)-min(y)))/(np.mean(y))*1e6 ))+" ppm" )
                     
-                    if(args.plot_bins):
+                    if(args.plot_bin_w):
                         ax.set_xlabel("Bin width [ns]", fontsize=font_size); ax.set_xlim(min(x)*0.9, max(x)*1.1)
-                        ax2=ax.twiny()
-                        # x2=np.flip(plot_data["ndf"])
-                        x2=plot_data["ndf"]
-                        ax2.set_xlabel("DoF (number of bins)", fontsize=font_size-2)
-                        ax2.plot(x2, y, lw=0)
+                        # ax2=ax.twiny()
+                        # # x2=np.flip(plot_data["ndf"])
+                        # x2=plot_data["ndf"]
+                        # ax2.set_xlabel("DoF (number of bins)", fontsize=font_size-2)
+                        # ax2.plot(x2, y, lw=0)
 
 
-                    if(args.plot_p_min): ax.set_xlabel(r"$p_{\rm{min}}$ [MeV]", fontsize=font_size);
-                    if(args.plot_p_both): 
+                    if(args.plot_p_min): ax.set_xlabel(r"$p_{\rm{min}}$ [MeV]", fontsize=font_size);  ax.set_xlim(min(x)*0.95, max(x)*1.05)
+                    if(args.plot_p_minp_max): 
                         ax.set_xlabel(r"$p$ [MeV]", fontsize=font_size) 
                         for tick in ax.get_xticklabels():
                             tick.set_rotation(45)
                     ax.set_ylabel(par_labels[i_key][i_par], fontsize=font_size);
                     # ax.legend(fontsize=font_size, loc='upper center', bbox_to_anchor=(0.5, 1.1))
                     ax.legend(fontsize=font_size, loc="best")
-                    fig.savefig("../fig/scans_fom/"+direction+"_"+key+"_"+par_names[i_key][i_par]+"_S"+str(station)+"_"+str(ds)+".png", dpi=300, bbox_inches='tight');
+                    fig.savefig("../fig/scans_fom/"+dirName+"/"+key+"_"+par_names[i_key][i_par]+"_S"+str(station)+"_"+str(ds)+".png", dpi=300, bbox_inches='tight');
 
                     # look into parameters
                     #if(par_names[i_key][i_par]=='A_cbo'): print(y, y_e, y_s); sys.exit()
+
+    #when done reading the file - backup
+    subprocess.call(["mv", "../DATA/scans/edm_scan_count.csv", "../DATA/scans/edm_scan_count_"+dirName+".csv"]) # backup previous file
+    subprocess.call(["mv", "../DATA/scans/edm_scan_theta.csv", "../DATA/scans/edm_scan_theta_"+dirName+".csv"]) # backup previous file
             
 def corr():
     '''
